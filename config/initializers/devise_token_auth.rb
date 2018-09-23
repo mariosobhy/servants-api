7de0073ad62b1c1140739db7fb4ab9c57ed81dcc
michaelfahmy <michaelfahmy1@gmail.com>
2018-09-23 04:48:14 +0200

    Revert "Added secret key base to devise token auth"
    This reverts commit 902975604a33b48b0bd1a72739061f7843b4b761.

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,6 +1,4 @@
 DeviseTokenAuth.setup do |config|
-  config.secret_key = ENV['SECRET_KEY_BASE']
-
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
