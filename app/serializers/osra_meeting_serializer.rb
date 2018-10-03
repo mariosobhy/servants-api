10fbc8a5dca4c6f87184cee14fa7ceb282da5910
michaelfahmy <michaelfahmy1@gmail.com>
2018-10-03 04:26:49 +0200

    Removed redundant id from osra meeting serializer

--- app/serializers/osra_meeting_serializer.rb
+++ app/serializers/osra_meeting_serializer.rb
@@ -1,6 +1,6 @@
 class OsraMeetingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :start_date, :end_date, :repeat
+  attributes :name, :start_date, :end_date, :repeat
 
   attribute :from do |object|
     object.from.strftime('%H:%M')
