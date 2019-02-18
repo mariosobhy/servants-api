cee8e5edd4e1119b823d0751ed7fbec443f14036
Mario Sobhy <mariosobhy0123@outlook.com>
2019-02-18 02:26:49 +0000

    Add count for number of sevants

--- app/models/conference_servant.rb
+++ app/models/conference_servant.rb
@@ -1,5 +1,5 @@
 class ConferenceServant < ApplicationRecord
-  belongs_to :conference
+  belongs_to :conference, counter_cache: :no_of_servants
   belongs_to :user
 
   validates :user, uniqueness: { scope: :conference, message: 'Servant has already been taken!' }
