b0dbbbf257c799b8ccf9f2516975b3c5557e406c
Mario Sobhy <mariosobhy0123@outlook.com>
2019-02-18 02:46:03 +0000

    add no of attendees

--- db/migrate/20181017233029_create_conferences.rb
+++ db/migrate/20181017233029_create_conferences.rb
@@ -2,9 +2,9 @@ class CreateConferences < ActiveRecord::Migration[5.2]
   def change
     create_table :conferences do |t|
       t.string :name
-      t.float :price
+      t.float :price, default: 0.0
       t.string :place
-      t.integer :no_of_servants
+      t.integer :no_of_servants, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_232536) do
+ActiveRecord::Schema.define(version: 2019_02_18_022940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,11 +75,12 @@ ActiveRecord::Schema.define(version: 2019_02_13_232536) do
 
   create_table "conferences", force: :cascade do |t|
     t.string "name"
-    t.float "price"
+    t.float "price", default: 0.0
     t.string "place"
-    t.integer "no_of_servants"
+    t.integer "no_of_servants", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "no_of_attendees", default: 0
   end
 
   create_table "course_meetings", force: :cascade do |t|
