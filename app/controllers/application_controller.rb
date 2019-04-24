e3499127b99f53757b3c1e797eaed8555ae5e5d5
michaelfahmy <michaelfahmy1@gmail.com>
2019-04-24 03:18:12 +0200

    Removed before action callback to set headers in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,4 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :authenticate_user!, unless: :devise_controller?
-  before_action :set_headers 
-
-  private 
-
-  def set_headers 
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    headers['Access-Control-Max-Age'] = '86400'
-  end 
 end
